40a5f4a5acbdb137bd11856d5c5671e4fcb6571f
Diego <diego.rocha@edu.unipar.br>
2024-03-03 17:53:36 -0300

    Update da queda do passaro

--- jogo.js
+++ jogo.js
@@ -11,6 +11,12 @@ const flappyBird = {
     altura: 24,
     x: 10,
     y: 50,
+    gravidade: 0.25,
+    velocidade: 0,
+    atuliza() {
+        flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade;
+        flappyBird.y = flappyBird.y + flappyBird.velocidade;
+    },
     desenha() {
         contexto.drawImage(
             sprites,
@@ -82,11 +88,11 @@ const fundo = {
 
 
 function loop() {
+    flappyBird.atuliza();
+
     fundo.desenha();
     chao.desenha();
     flappyBird.desenha();
-
-    
 
 
     requestAnimationFrame(loop);
