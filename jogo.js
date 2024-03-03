55305fba7b7c146b7db09a8c6c61f74b216cf81b
Diego <diego.rocha@edu.unipar.br>
2024-03-03 15:58:27 -0300

    Desenhado componentes

--- jogo.js
+++ jogo.js
@@ -3,3 +3,93 @@ sprites.src = './sprites.png';
 
 const canvas = document.querySelector('canvas');
 const contexto = canvas.getContext('2d');
+
+const flappyBird = {
+    spriteX: 0,
+    spriteY: 0,
+    largura:34,
+    altura: 24,
+    x: 10,
+    y: 50,
+    desenha() {
+        contexto.drawImage(
+            sprites,
+            flappyBird.spriteX, flappyBird.spriteY, // Sprite X, Sprit Y
+            flappyBird.largura, flappyBird.altura, // Tamanho do recorte na sprite
+            flappyBird.x, flappyBird.y,
+            flappyBird.largura, flappyBird.altura,
+        );
+    }
+}
+
+// [Chao]
+const chao = {
+    spriteX: 0,
+    spriteY: 610,
+    largura: 224,
+    altura: 113,
+    x: 0,
+    y: canvas.height - 113,
+    desenha() {
+        contexto.drawImage(
+            sprites,
+            chao.spriteX, chao.spriteY,
+            chao.largura, chao.altura,
+            chao.x, chao.y,
+            chao.largura, chao.altura
+        );
+
+        contexto.drawImage(
+            sprites,
+            chao.spriteX, chao.spriteY,
+            chao.largura, chao.altura,
+            (chao.x + chao.largura), chao.y,
+            chao.largura, chao.altura
+        )
+    }
+}
+
+// [Plano de Fundo]
+const fundo = {
+    spriteX: 390,
+    spriteY: 0,
+    largura: 276,
+    altura: 204,
+    x: 0,
+    y: canvas.height - 204,
+    desenha() {
+        contexto.fillStyle = '#70c5ce';
+        contexto.fillRect(0,0, canvas.width, canvas.height);
+
+        contexto.drawImage(
+            sprites,
+            fundo.spriteX, fundo.spriteY,
+            fundo.largura, fundo.altura,
+            fundo.x, fundo.y,
+            fundo.largura, fundo.altura
+        );
+
+        contexto.drawImage(
+            sprites,
+            fundo.spriteX, fundo.spriteY,
+            fundo.largura, fundo.altura,
+            (fundo.x + fundo.largura), fundo.y,
+            fundo.largura, fundo.altura
+        );
+    }
+}
+
+
+
+function loop() {
+    fundo.desenha();
+    chao.desenha();
+    flappyBird.desenha();
+
+    
+
+
+    requestAnimationFrame(loop);
+}
+
+loop();
